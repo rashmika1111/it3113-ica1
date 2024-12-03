edff16e0956556a10e11ba5d5b102660844c590e
rashmika1111 <139681232+rashmika1111@users.noreply.github.com>
2024-12-03 21:05:14 +0530

    created product.js

--- ica1/src/components/Product.js
+++ ica1/src/components/Product.js
@@ -4,15 +4,16 @@ export default function Product(){
     return(
         <div className="grid-item">
 
-            <div class="card">
-                <img  />
+           
+<div class="card">
+                <img src={require(`../assets/image/${flower.img}`)} alt={flower.name} />
                 <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
+                    <h5 class="card-title">{flower.name} Price: {flower.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+                        <input type="number" id="quantity" name="quantity" onChange={(e)=>(flower.qty=+e.target.value)} />
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={()=>onAddToCart(flower)}>Add to Cart</button>
                 </div>
             </div>
         </div>
